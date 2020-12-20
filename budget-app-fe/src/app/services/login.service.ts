a273b502273306912f0af5ae8e6552f7b2fad6a9
Dylan Eddies <dylan.eddies1@gmail.com>
2020-12-20 03:06:29 +0000

    package updates

--- budget-app-fe/src/app/services/login.service.ts
+++ budget-app-fe/src/app/services/login.service.ts
@@ -27,16 +27,17 @@ export class LoginService {
 
   private checkAuthenticationStatus() {
     const token = LoginService.readTokenCookie();
-    const value: any = JSON.parse(atob(token));
-    const currentDate = new Date();
-    const expires = new Date(value.accessTokenExpiresAt);
-    if(currentDate > expires) {
-      this.logout();
-    }
-    else if (token && token.length > 0) {
-      this.authToken = token;
-      this.authenticated = true;
-      this.emitAuthenticationStatus(true);
+    if(token && token.length > 0) {
+      const value: any = JSON.parse(atob(token));
+      const currentDate = new Date();
+      const expires = new Date(value.accessTokenExpiresAt);
+      if (currentDate > expires) {
+        this.logout();
+      } else {
+        this.authToken = token;
+        this.authenticated = true;
+        this.emitAuthenticationStatus(true);
+      }
     }
   }
 
